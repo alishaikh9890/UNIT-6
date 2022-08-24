4a0f2fd076a09d7fefac192d744836a2a3bfcd85
Ali-9890 <alishanshaikh9890@gmail.com>
2022-08-24 19:08:40 +0530

    assignment

--- ASSIGNMENTS/todo/src/App.js
+++ ASSIGNMENTS/todo/src/App.js
@@ -1,9 +1,45 @@
 
 import './App.css';
+import React from "react"
+import List from "./List.js"
 
 function App() {
+
+const [counter, setCounter]= React.useState(20)
+
+const [details, setDetails]= React.useState("SHOW MORE DETAILS")
+
+const handleChange= (value) =>{
+setCounter(counter+value)
+}
+
+
+const Details =(v) => {
+ if(details=="SHOW MORE DETAILS"){
+  setDetails("HIDE MORE DETAILS")
+ }
+ else if(details=="HIDE MORE DETAILS"){
+  setDetails("SHOW MORE DETAILS")
+ }
+}
+
   return (
     <div className="App">
+      <div className='Profile'>
+      <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKPT2rDdS2mqmvOrCi8l7gutEQuqqsFyzOzKgHtW1MKg&s' alt=''/>
+      <h2>Name: Alishan Shaikh</h2>
+      <h2>Age: {counter}</h2>
+
+      <button onClick={()=> handleChange(1)}>INCREASE AGE</button>
+      <button onClick={()=> handleChange(-1)} >DECREASE AGE</button>
+      <button onClick={() =>Details()}>{details}</button> 
+      {details=="HIDE MORE DETAILS" ?  <div><h2>Location: Pune</h2><h2>Organization:"MASAI"</h2></div> : <div></div>}
+    
+      </div>
+
+      <div className='todo'>
+       <List />
+      </div>
       
     </div>
   );
---
+++ ASSIGNMENTS/todo/src/List.js
@@ -0,0 +1,27 @@
+
+import React from 'react';
+
+
+function List(){
+
+ const list = [
+    { id: 1, title: "Wake up", status: true },
+    { id: 2, title: "drink tea", status: false },
+    { id: 3, title: "eat maggi", status: true },
+    { id: 4, title: "sleep", status: false }
+  ]
+
+    return (
+        <div>
+        <h1>Todo List</h1>
+   
+    {list.map(el=>(
+        el.status==false ? <p style={{color:"red"}}>{el.title}</p> :<p style={{color:"green"}}>{el.title}</p>
+    ))}
+   
+
+</div>
+    )
+}
+
+  export default List;
