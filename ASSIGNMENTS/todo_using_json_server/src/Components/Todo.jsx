8e3c362e8f90d0430938206acfd489e753f3ee85
Ali-9890 <alishanshaikh9890@gmail.com>
2022-08-30 18:53:58 +0530

    .

--- ASSIGNMENTS/todo_using_json_server/src/Components/Todo.jsx
+++ ASSIGNMENTS/todo_using_json_server/src/Components/Todo.jsx
@@ -1,47 +1,53 @@
 import React from "react";
 
-
  export const Todo = () =>{
     const [inputValue, setInputValue] = React.useState("");
     const [loading, setLoading]= React.useState(false);
     const [todos, setTodos] = React.useState([]);
-    const [error, setError] = React.useState(false);
     const [page,setPage] = React.useState(1);
+    const [totalItems, setTotalItems] =React.useState(0)
+    const [error, setError] = React.useState(false);
     
-
+    
     React.useEffect(() => {
         fetchAndUpdateData()
-    },[])
+    },[page])
 
-    const fetchAndUpdateData =() => {
+    const fetchAndUpdateData = () => {
         setLoading(true);
-        fetch(`http://localhost:3008/tasks`)
-        .then((res) => res.json())
+        fetch(` http://localhost:3008/tasks?_page=${page}&_limit=3`)
+        .then((res) => {
+            let total = res.headers.get("X-Total-Count");
+            setTotalItems(+total);
+            return res.json();
+        })
+        
         .then((res) => setTodos(res))
         .catch((err) => setError(true))
         .finally(() => setLoading(false));
-        
     };
 
     const addTodo = () => {
-        const payload ={
+        const payload = {
             title: inputValue,
             status: false,
-        }
+        };
+
         const dataToPost = JSON.stringify(payload);
 
-        fetch(`http://localhost:3008/tasks`, {
+        fetch(` http://localhost:3008/tasks`, {
             method: "POST", 
             body: dataToPost,
             headers: {
                 "Content-Type" : "application/json"
             },
         }).then(() => {
-            fetchAndUpdateData()
+            fetchAndUpdateData();
+            setInputValue("");
         })
     }
 
-     
+      
     return(
         
         <div className="todo">
@@ -49,19 +55,32 @@ import React from "react";
             <input 
             type="text"
              placeholder="Add the new Todo" 
-             onClick={(e) => setInputValue(e.target.value)}
+             onChange={(e) => setInputValue(e.target.value)}
+             value={inputValue}
              />
              <button onClick={addTodo} >Add</button>
-
+            <hr/>
              {loading ? (
              <div>Loading...</div>
             ) : error ? (
                 <div>something went wrong</div>
             ) : (
-                todos.map((todo) => <div key={todo.id}>{todo.title}</div>)
-            )}
-        
-            
+                <div>
+               { todos.map((todo) => (
+               <div key={todo.id}>{todo.title}</div>
+                ))}
+                <button onClick={() => setPage(page - 1)} disabled={page===1} >
+                    PREV
+                    </button>
+                <button
+                 onClick={() => setPage(page + 1)}
+                  disabled={page=== Math.ceil(totalItems / 3)} 
+                 
+                 >
+                    NEXT
+                    </button>
+                </div>
+                )}
         </div>
     )
 }
